d58db4be2286ceb118781f5593ceb27b59d6602c
Nishad <avnishkumar1st@gmail.com>
2018-11-07 21:24:00 +0530

    add some more color

--- app/src/main/java/com/example/avnish/tictoc/MainActivity.java
+++ app/src/main/java/com/example/avnish/tictoc/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.avnish.tictoc;
 
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getin(View view) {
-
+        TextView txtvw=(TextView)findViewById(R.id.textview);
         ImageView img = (ImageView) view;
         int imageId = Integer.parseInt(img.getTag().toString());
         img.setTranslationZ(-1000f);
@@ -29,12 +31,16 @@ public class MainActivity extends AppCompatActivity {
         //one can not click on pre occupied block using this IF()
         if(arr[imageId]==2) {
             if (activeplayer == 0) {
+                txtvw.setText("YELLOW PLAYER");
+                txtvw.setTextColor((Color.YELLOW));
                 arr[imageId] = 1;
                 img.setImageResource(R.drawable.yellow);
                 img.animate().translationZBy(1000f).rotation(360).setDuration(2000);
                 activeplayer = 1;
 
             } else {
+                txtvw.setText("RED PLAYER");
+                txtvw.setTextColor(Color.RED);
                 arr[imageId] = 3;
                 img.setImageResource(R.drawable.red);
                 img.animate().translationZBy(1000f).rotation(360).setDuration(2000);
